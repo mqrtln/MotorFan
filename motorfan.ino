cb77aab6477f4b37a642e3f522912c53ce03d606
mqrtln <mart0l@outlook.com>
2022-10-13 20:56:55 +0200

    making things actually work

--- motorfan.ino
+++ motorfan.ino
@@ -1,25 +1,22 @@
-#define RIGHT_MOTOR 1
-#define LEFT_MOTOR 7
+#define COUNTER_CLOCKWISE_MOTOR 2
+#define CLOCKWISE_MOTOR 7
 #define MOTOR_ENABLED 8
 
-int VRx = A0; 
 int VRy = A1; 
 
-int xPosition = 0; 
 int yPosition = 0; 
-int mapX = 0; 
+
 int mapY = 0; 
 
 void setup(){
     Serial.begin(9600); 
 
     // Pins for the LD motor
-    pinMode(RIGHT_MOTOR, OUTPUT);
-    pinMode(LEFT_MOTOR, OUTPUT);
+    pinMode(COUNTER_CLOCKWISE_MOTOR, OUTPUT);
+    pinMode(CLOCKWISE_MOTOR, OUTPUT);
     pinMode(MOTOR_ENABLED, OUTPUT);
 
     // Pins for the joystick
-    pinMode(VRx, INPUT);
     pinMode(VRy, INPUT);
 }
 
@@ -27,44 +24,29 @@ void loop(){
 
 
 
-    xPosition = analogRead(VRx); 
     yPosition = analogRead(VRy);
 
 
 
-    mapX = map(xPosition, 0, 1023, -512, 512); 
     mapY = map(yPosition, 0, 1023, -512, 512); 
 
-
-
-
+    digitalWrite(MOTOR_ENABLED, HIGH); 
     // RIGHT & LEFT
-    if(mapY == 512){
+    if(mapY == 510){
         Serial.println("LEFT");
-        rotateMotor("LEFT", HIGH); 
+        digitalWrite(COUNTER_CLOCKWISE_MOTOR, HIGH);
+        digitalWrite(CLOCKWISE_MOTOR, LOW);
     }
-    if(mapY == -512){
-        Serial.print("RIGHT");
-        rotateMotor("RIGHT", HIGH); 
+    if(mapY == -510){
+        Serial.println("RIGHT");
+        digitalWrite(CLOCKWISE_MOTOR, HIGH);
+        digitalWrite(COUNTER_CLOCKWISE_MOTOR, LOW);
     } else{
-        rotateMotor("RIGHT", LOW); 
-        rotateMotor("LEFT", LOW); 
+       
+       digitalWrite(MOTOR_ENABLED, LOW); 
     }
 
 
 }
 
-void rotateMotor(String motordir, int enabledOrDisabled){
-    if(motordir == "RIGHT"){
-        Serial.println("Spinning motor to the right"); 
-        digitalWrite(MOTOR_ENABLED, enabledOrDisabled); 
-        digitalWrite(RIGHT_MOTOR, HIGH); 
-        digitalWrite(LEFT_MOTOR, LOW); 
-    }else{
-        Serial.println("Rotating the motor to the left"); 
-        digitalWrite(MOTOR_ENABLED, enabledOrDisabled); 
-        digitalWrite(RIGHT_MOTOR, LOW); 
-        digitalWrite(LEFT_MOTOR, HIGH); 
-    }
-}
 
